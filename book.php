6e9db700ccb3349ec03d9369e26db9b8493c3433
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-05 19:39:39 +0100

    Creo la classe libro
    1 - Definisco le proprietà della classe
    2 - Inserisco la funzione construct

--- book.php
+++ book.php
@@ -1,3 +1,22 @@
 <?php 
-  // ...
+  
+  class Book {
+    // Definisco le proprietà della classe libro
+    public $title;
+    public $author;
+    public $genre;
+    public $cover;
+    public $publication_year;
+    public $isbn_code;
+
+    // Inserisco la funzione construct
+    public function _construct($title,$author,$genre,$cover,$publication_year,$isbn_code) {
+      $this->title = $title;
+      $this->author = $author;
+      $this->genre = $genre;
+      $this->title = $title;
+      $this->title = $title;
+    }
+  }
+
 ?>
