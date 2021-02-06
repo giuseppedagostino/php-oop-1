2e87cc974ebde697bfa9aa62ce27996d24bd506d
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-06 10:11:58 +0100

    Import dell'array di libri

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+<!-- includo il database -->
+<?php require __DIR__."/book.php" ?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php-oop-1</title>
+  <!-- font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+  <!-- css -->
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
   <h1>php-oop-1</h1>
+  <?php var_dump($books); ?>
 </body>
 </html>
