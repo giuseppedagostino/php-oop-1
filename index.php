c699b8c2b3ff9d29b4ed4476bdcc8399dc46e7b8
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-06 13:07:32 +0100

    Altri Fix
    1 - Stampa dei valori con foreach
    2 - Utilizzo del $key -> $value

--- index.php
+++ index.php
@@ -4,24 +4,28 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>php-oop-1</title>
-  <!-- font -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
-  <!-- css -->
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>php-oop-1</title>
 </head>
 
 <body>
+  <?php var_dump($books); ?>
   <h1>php-oop-1</h1>
   <div class="container">
-  <?php foreach($books as $book): ?>
-    <div class="book">
-      <?php var_dump($book) ?>
-    </div>
-  <?php endforeach; ?>
+    <?php foreach ($books as $key => $value): ?>
+      <div class="book">
+        <h3><?= $value->title; ?></h3>
+        <h4><?= $value->author; ?></h4>
+        <h5><?= $value->author; ?></h5>
+        <h5><?= $value->author; ?></h5>
+      </div>
+    <?php endforeach; ?>
   </div>
 </body>
 </html>
